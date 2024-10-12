258665124fa12aaab8b46039ac4ab31456acc176
zellyo-digital0 <zellyo.digital@gmail.com>
2024-10-12 18:23:12 +0600

    v1

---
+++ js/accordian_icon.js
@@ -0,0 +1,31 @@
+$("#headingOne").click(function () {
+    $(".rotate1").toggleClass("rotated");
+});
+
+$("#headingTwo").click(function () {
+    $(".rotate2").toggleClass("rotated");
+});
+
+$("#headingThree").click(function () {
+    $(".rotate3").toggleClass("rotated");
+});
+
+$("#headingFour").click(function () {
+    $(".rotate4").toggleClass("rotated");
+});
+
+$("#headingFive").click(function () {
+    $(".rotate5").toggleClass("rotated");
+});
+
+$("#headingSix").click(function () {
+    $(".rotate6").toggleClass("rotated");
+});
+
+$("#headingSeven").click(function () {
+    $(".rotate7").toggleClass("rotated");
+});
+
+$("#headingEight").click(function () {
+    $(".rotate8").toggleClass("rotated");
+});
